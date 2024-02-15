ee5665b672efb62db5c88d64e957e9f38e6393ec
Manavesh Narendra <manavesh.narendra@gmail.com>
2024-02-15 22:22:21 +0530

    Added Model Class to Application

--- src/main/java/com/learn/springbootdemo/ui/controller/UserController.java
+++ src/main/java/com/learn/springbootdemo/ui/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.learn.springbootdemo.ui.controller;
 
+import com.learn.springbootdemo.ui.entity.UserEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,8 +11,23 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping
-    public String getUser() {
-        return "Get User was Called";
+    public String getUsers(
+            @RequestParam(value = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(value = "limit", required = false, defaultValue = "25") int limit) {
+        return String.format("getUser was called with page : %d and limit : %d", page, limit);
+    }
+
+    @GetMapping(
+            path = "/{userId}",
+            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<UserEntity> getUser(@PathVariable String userId) {
+
+        UserEntity userEntityObject = new UserEntity();
+        userEntityObject.setFirstName("Manavesh");
+        userEntityObject.setLastName("Narendra");
+        userEntityObject.setAge(24);
+
+        return new ResponseEntity<UserEntity>(userEntityObject, HttpStatus.OK);
     }
 
     @PostMapping
